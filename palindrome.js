62fc5e6727a3e68df7037e21857957e270a3adf9
akki9689 <akashpuri259@gmail.com>
2022-12-04 20:50:47 +0530

    Added prime no and palindrome

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+let str="naman"
+rev=""
+for(let i=str.length-1;i>=0;j--){
+rev+=str[j];
+}if(str==rev){
+console.log("Plimdrome")
+}else{
+console.log("Not Palindrome")}
---
+++ prime.js
@@ -0,0 +1,13 @@
+
+let num=13;
+let count=0;
+for(let i=1;i<=num;i++){
+if(num%num==0){
+count++;
+}
+}
+if(count==1){
+console.log("Prime no.")
+}else{
+console.log("Non-Prime no.")
+}
