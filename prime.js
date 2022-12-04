72bbfbb6889fc72bfd3d7d3b599c53d05811dc79
appuk23 <apurvakamble2306@gmail.com>
2022-12-04 21:51:04 +0530

    checked ok

--- prime.js
+++ prime.js
@@ -2,11 +2,11 @@
 let num=13;
 let count=0;
 for(let i=1;i<=num;i++){
-if(num%num==0){
+if(num%i==0){
 count++;
 }
 }
-if(count==1){
+if(count==2){
 console.log("Prime no.")
 }else{
 console.log("Non-Prime no.")
